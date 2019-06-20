a12ccd4f8569339e94ad6f8438b0f8fa9500df2f
pj03 <52020613+pj03@users.noreply.github.com>
2019-06-19 22:29:16 -0500

    ML Elements render
    multiple elements render

--- src/index.js
+++ src/index.js
@@ -1,16 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+// Multiple elements Render
+let d = document.getElementById("a1");
+var e = (
+  <div>
+    <h1>H1 TAG</h1>
+    <h2>H2Tag</h2>
+    <h3>h3 tag</h3>
+  </div>
+);
+
+ReactDOM.render(e, d);
+
+/*
+// Single element render
 let a = <h1>Hi This is H1 Tag</h1>;
 let b = document.getElementById("a1");
-
-//ReactDOM.render(a,b);
-
 ReactDOM.render(a, b);
 
-//import "./styles.css";
+*/
 
-/*function App() {
+/*import "./styles.css";
+function App() {
 return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
